98aed3da835ad95433311d4be21ebfc4ceed5c0f
Michal Trojanowski <mtroja98@gmail.com>
2026-08-02 15:34:14 +0200

    Cancellation texts

--- src/app/components/tables/table-reservations-user/table-reservations-user.ts
+++ src/app/components/tables/table-reservations-user/table-reservations-user.ts
@@ -38,32 +38,39 @@ export class TableReservationsUser {
   isCancellationPossible(res: ReservationDto): boolean {
     if (
       res.status === ReservationStatus.CANCELLED ||
+      res.status === ReservationStatus.REJECTED ||
       res.status === ReservationStatus.REQUESTED_CANCELLATION
     ) {
       return false;
     }
 
+    return !this.isTooLateToCancel(res);
+  }
+
+  isTooLateToCancel(res: ReservationDto) {
     const startDate = new Date(res.startAt);
     const now = new Date();
     const timeDifference = startDate.getTime() - now.getTime();
     const hoursDifference = timeDifference / (1000 * 60 * 60);
-    return hoursDifference >= 24 || res.status === ReservationStatus.CREATED;
+    return hoursDifference <= 24;
   }
 
   getCancelButtonLabel(res: ReservationDto): string {
-    if (!this.isCancellationPossible(res)) {
-      return this.translocoService.translate('PROFILE.CANCEL_BTN_DISABLED');
+    if (this.isTooLateToCancel(res)) {
+      return this.translocoService.translate('BUTTONS.TOO_LATE_TO_CANCEL');
     }
-
     switch (res.status) {
       case ReservationStatus.CREATED:
-        return this.translocoService.translate('PROFILE.CANCEL_BTN');
+        return this.translocoService.translate('BUTTONS.REQUEST_CANCEL');
       case ReservationStatus.CONFIRMED:
-        return this.translocoService.translate('PROFILE.CANCEL_BTN');
+        return this.translocoService.translate('BUTTONS.REQUEST_CANCEL');
       case ReservationStatus.CANCELLED:
-        return this.translocoService.translate('PROFILE.CANCEL_BTN');
+        return this.translocoService.translate('BUTTONS.CANCELLATION_NOT_POSSIBLE');
+      case ReservationStatus.REJECTED:
+        return this.translocoService.translate('BUTTONS.CANCELLATION_NOT_POSSIBLE');
       case ReservationStatus.REQUESTED_CANCELLATION:
-        return this.translocoService.translate('PROFILE.REQUESTED_CANCELLATION_BUTTON');
+        return this.translocoService.translate('BUTTONS.CANCELLATION_NOT_POSSIBLE');
+
       default:
         return '';
     }
@@ -84,6 +91,8 @@ export class TableReservationsUser {
         return this.translocoService.translate('STATUS.CANCELLED');
       case ReservationStatus.REQUESTED_CANCELLATION:
         return this.translocoService.translate('STATUS.REQUESTED_CANCELLATION');
+      case ReservationStatus.REJECTED:
+        return this.translocoService.translate('STATUS.REJECTED');
       default:
         return '';
     }
